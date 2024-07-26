2b93e3bc41ea510991065830917ddbd186f3623e
Ali Yahya <165564206+CodebyAliyah@users.noreply.github.com>
2024-07-25 19:33:04 -0700

    Code Done

---
+++ Control_Flow.js
@@ -0,0 +1,98 @@
+// if
+
+if (true) {
+  // code will be executed.
+}
+
+if (false) {
+  // code wel'not be executed.
+}
+
+// < , > , => , <= ,! , = , == , ===
+
+if (2!=3) {
+  console.log("Tge Code Will Be Executed");
+}
+
+else {
+  console.log("The Code Wil'not Be Executed");
+}
+
+
+
+
+
+const score = 300 ;
+if (score < 400) {
+  const power = "go"
+  console.log(`The power is : ${power}`)
+}
+
+// console.log(power) => agr hum powr ko if {} k nahir print kry gy to nahi hu ga q k uska scope is fineshed is k bahir khatam hu jata ha.
+
+const balance = 1000;
+if (balance > 500) {
+  console.log("The code will be executed")
+}
+else {
+  console.log("The code wil'not be executed")
+}
+
+
+
+const pubGloggedIn = true ;
+const payMission = true ;
+
+if (pubGloggedIn && payMission) {
+  console.log("The Code Will Be Executed");
+}
+
+
+const userLoggedInGoogle = false ;
+
+const userLoggedInEmail = true ;
+if (userLoggedInGoogle || userLoggedInEmail) {
+  console.log("CONGRAGULATION")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
